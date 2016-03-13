e6acb58ac85b1543696769cbd7704c15938a920f
maxbittker <Max Bittker>
2016-03-12 21:08:03 -0500

    scraped

--- scrape.js
+++ scrape.js
@@ -9,28 +9,29 @@ let ArticleInfo = []
 
 function scrapeIndex(i) {
     console.log("scraping " + i)
+
     request(baseUrl + "editorials/?page=" + i, function (err, res, bod) {
         if (err) {
             throw err
         }
 
         let $ = cheerio.load(bod);
-        $('.view-content h2 a').each(function (i, elem) {
+        $('.view-content h2 a').each(function (index, elem) {
             c++
             scrapeArticle($(this).prop('href'), i)
         })
 
-        if (i <= 64) scrapeIndex(i + 1)
+        if (i < 64) scrapeIndex(i + 1)
     })
 }
 
-function scrapeArticle(url, i) {
+function scrapeArticle(url, index) {
     request(baseUrl + url, function (err, res, bod) {
         if (err) {
             throw err
         }
-        if (res !== 200 || !bod) {
-            console.log(url + " FAILED !!!!!!!!!!!")
+        if (res.statusCode !== 200 || !bod) {
+            console.log(url + res.statusCode + " FAILED !!!!!!!!!!!")
             return
         }
         let $ = cheerio.load(bod)
@@ -40,11 +41,12 @@ function scrapeArticle(url, i) {
             return $(this).text()
         }).get().toString()
         ArticleInfo.push({
-            title, date, content
+            title, url, date, content
         })
         c--
-        if (c === 0 && i === 64) {
-            fs.writeFile("output.js", JSON.serialize(ArticleInfo), (error) => {
+        console.log(c,index,title)
+        if (c === 0 && index === 64) {
+            fs.writeFile("output.js", JSON.stringify(ArticleInfo), (error) => {
                 if (err) throw err
             })
         }
