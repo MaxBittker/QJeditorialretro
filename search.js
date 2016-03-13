d4e739793e8660340850ef8d7f8efe2bb7700ec1
maxbittker <Max Bittker>
2016-03-13 14:28:27 -0400

    web interface

---
+++ search.js
@@ -0,0 +1,24 @@
+"use strict"
+let fs = require('fs')
+let _ = require('lodash')
+const data = JSON.parse(fs.readFileSync('output.js'))
+console.log(data.length)
+
+
+function search(keywords){
+
+  let re = new RegExp(keywords.join("|"),"i");
+  let o = data.filter(entry=>re.test(entry.content))
+  let mapped = o.map(entry=>{
+    let d = new Date(entry.date)
+    let yr = d.getFullYear()
+    return {title:entry.title,
+            year:yr}
+    })
+  let grouped = _.groupBy(mapped,entry=>entry.year)
+  console.log(grouped)
+}
+
+// search(["mental health", "anxiety"])
+// search(["race", "racism"])
+search(["assault", "rape"])
